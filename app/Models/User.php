4aa55016be2a47a61cb87ac3a5f2800f993963a8
Jagga-max117 <78495607+Jagga-max117@users.noreply.github.com>
2024-07-25 11:23:38 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
