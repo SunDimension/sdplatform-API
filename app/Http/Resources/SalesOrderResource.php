eafb477ccecc9d85977c8fc7f317dcafdb3b048b
Jagga-max117 <78495607+Jagga-max117@users.noreply.github.com>
2024-10-05 09:57:44 +0100

    Add files via upload

--- app/Http/Resources/SalesOrderResource.php
+++ app/Http/Resources/SalesOrderResource.php
@@ -14,6 +14,16 @@ class SalesOrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'customer_id' => $this->customer_id,
+            'product_id' => $this->product_id,
+            'branch_id' => $this->branch_id,
+            'store_id' => $this->store_id,
+            'sales_order_number' => $this->sales_order_number,
+            'credit_limit' => $this->credit_limit,
+            'credit_balance' => $this->credit_balance,
+            'total_amount' => $this->total_amount,
+        ];
     }
 }
