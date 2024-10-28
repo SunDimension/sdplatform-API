e766bb0c612aa1b890cf6463ebf8dbda346a8085
Jagga-max117 <78495607+Jagga-max117@users.noreply.github.com>
2024-10-28 17:17:38 +0100

    #

--- app/Http/Resources/StoreItemResource.php
+++ app/Http/Resources/StoreItemResource.php
@@ -17,17 +17,23 @@ class StoreItemResource extends JsonResource
         return [
             'id' => $this->id,
             'item_category_id'=>$this->item_category_id,
+            // 'item_category_name' => $this->item_category ? $this->item_category->name : null,
             'create_item_id'=>$this->create_item_id,
+            // 'create_item_name' => $this->create_item ? $this->create_item->name : null,
             'unit_id'=>$this->unit_id, 
+            // 'unit_name' => $this->unit ? $this->unit->name : null,
             'cost_price'=>$this->cost_price, 
             'selling_price'=>$this->selling_price, 
             'reorder_level'=>$this->reorder_level, 
             'quantity'=>$this->quantity - $this->quantity_holding, 
             'quantity_holding'=>$this->quantity_holding, 
-            'store_id'=>$this->store_id,  
+            'store_id'=>$this->store_id,
+            // 'store_name' => $this->store ? $this->store->name : null,  
             'user_id'=>$this->user_id,
+           
             'discount'=>$this->discount,
-            'store_type_id'=>$this->store_type_id
+            'store_type_id'=>$this->store_type_id,
+            // 'store_type_name' => $this->store_type ? $this->store_type->name : null,
         ];
     }
 }
