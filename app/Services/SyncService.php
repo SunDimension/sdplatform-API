12cfe01cd72bb699fcc0ef4edc2aaa114dbe2a6d
Joe Onyilo <onyilo@gmail.com>
2025-08-24 14:58:34 +0100

    Updates

--- app/Services/SyncService.php
+++ app/Services/SyncService.php
@@ -398,8 +398,14 @@ class SyncService
             
             foreach ($models as $model) {
                 try {
+                    // Format the data as a proper sync change that pullChanges expects
                     $syncData = $this->prepareModelForHub($model);
-                    $results['data'][] = $syncData;
+                    $change = [
+                        'model_type' => get_class($model),
+                        'action' => $this->determineModelAction($model),
+                        'data' => $syncData
+                    ];
+                    $results['data'][] = $change;
                 } catch (Exception $e) {
                     Log::error('Failed to prepare model for hub', [
                         'model' => get_class($model),
